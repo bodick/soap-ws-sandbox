2806c2d1f8847d99e075cb158b8a813969ffe3ec
bodick <jlkwison@gmail.com>
2013-01-01 19:18:15 +0800

    initial commit

---
+++ src/com/jlkwison/services/CartService.java
@@ -0,0 +1,24 @@
+package com.jlkwison.services;
+
+public class CartService {
+
+    public CartService() {
+    }
+
+    public Boolean Configure(String param, String val) {
+        return true;
+    }
+
+    public Boolean StartMe() {
+        return true;
+    }
+
+    public Boolean StopMe() {
+        return true;
+    }
+
+    public String getStatus() {
+        return "No status available!";
+    }
+    
+}
